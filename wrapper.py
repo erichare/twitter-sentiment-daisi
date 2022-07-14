0d3358557e262f42e496324e0f794b4c27f6a62b
Eric Hare <ericrhare@gmail.com>
2022-07-13 17:28:55 -0700

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -2,8 +2,8 @@ import pydaisi as pyd
 import pandas as pd
 import streamlit as st
 
-tw_daisi = pyd.Daisi("erichare/Twitter Search", instance="dev3")
-sa_daisi = pyd.Daisi("erichare/Sentiment Analysis", instance="dev3")
+tw_daisi = pyd.Daisi("erichare/Twitter Search")
+sa_daisi = pyd.Daisi("erichare/Sentiment Analysis")
 
 def get_twitter_sentiment(query: str, count: int=10):
     '''
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     st.set_page_config(layout = "wide")
     st.title("Twitter Sentiment Analysis with Daisies")
 
-    st.markdown("This Wrapper Daisi calls two other Daisies, the [Twitter Search](https://dev3.daisi.io/daisies/4602e8b6-127b-4d1f-b13c-60d7b7b7431b/info) Daisi, and the [Sentiment Analysis](https://dev3.daisi.io/daisies/dd7ca16b-efeb-47b4-80ca-2f77ef106739/info) Daisi, to perform a Sentiment Analysis of the ten most recent tweets related to your query!")
+    st.markdown("This Wrapper Daisi calls two other Daisies, the [Twitter Search](https://app.daisi.io/daisies/5636b873-4ed9-44c2-a737-ad9b95dedfba/info) Daisi, and the [Sentiment Analysis](https://dev3.daisi.io/daisies/dd7ca16b-efeb-47b4-80ca-2f77ef106739/info) Daisi, to perform a Sentiment Analysis of the ten most recent tweets related to your query!")
 
     with st.sidebar:
         query = st.text_input('Twitter Search Keyword', 'Daisi Python')
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         ```python
         import pydaisi as pyd
 
-        twitter_sentiment = pyd.Daisi("erichare/Twitter Sentiment", instance="dev3")
+        twitter_sentiment = pyd.Daisi("erichare/Twitter Sentiment")
         result = twitter_sentiment.get_twitter_sentiment("{query}").value
         
         result
